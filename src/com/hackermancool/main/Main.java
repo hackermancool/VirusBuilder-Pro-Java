2991e5a0987129e561597cddf00acb74ecbeb637
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2020-01-23 21:27:02 -0500

    Added GPL-3.0 attribution to code.

--- src/com/hackermancool/main/Main.java
+++ src/com/hackermancool/main/Main.java
@@ -1,4 +1,20 @@
-// Created by hackermancool on 20/01/2020.
+/*
+ * VirusBuilder Pro Java - A high-tech cross-platform virus creation tool written in Java that generates matching Batch and Shell viruses.
+ * Copyright (C) 2020  hackermancool
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.hackermancool.main;
 
 import java.awt.EventQueue;
